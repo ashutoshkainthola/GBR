c2a4022d7168a0d81f0cd05c28c066a4220c25ca
ashutoshkainthola <96884058+ashutoshkainthola@users.noreply.github.com>
2022-05-02 22:56:43 +0530

    Update regression.py
    GridSearchCV params

--- regression.py
+++ regression.py
@@ -7,7 +7,7 @@ df = pd.read_csv("Ann.csv")
 X= df.loc[:, df.columns != "FOS"]
 y = df["FOS"]
 
-GBR = GradientBoostingRegressor().fit(X,y)
+GBR = GradientBoostingRegressor(learning_rate= 0.1, n_estimators= 500).fit(X,y)
 GBR.score(X,y)
 
 
